4b40cdb00846205c14ffc10772b8793eb79e0b3b
XuKanglei <xukanglei@126.com>
2019-04-28 12:00:54 +0800

    UPDATE20190428

--- bookshop/src/cn/edu/nsu/bookshop/servlet/table.java
+++ bookshop/src/cn/edu/nsu/bookshop/servlet/table.java
@@ -1,9 +1,6 @@
 package cn.edu.nsu.bookshop.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Date;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class CurrentTime
+ * Servlet implementation class table
  */
-//@WebServlet("/CurrentTime")
-public class CurrentTime extends HttpServlet {
+//@WebServlet("/table")
+public class table extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public CurrentTime() {
+    public table() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,26 +27,7 @@ public class CurrentTime extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.setContentType("text/html;charset=utf-8");
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE html>");
-		out.println("<head>");
-		out.println("<meta charset=\"utf-8\">");
-		out.println("<title>当前时间</title>");
-		out.println("</head>");
-		out.println("<body>");
-		Date date = new Date();
-		String currtTime = date.toLocaleString();
-		out.println("当前时间    ");
-		out.println("<font color=\"red\">");
-		out.println(currtTime);
-		out.println("</font>");
-		out.println("</body>");
-		out.println("</html>");
-		
-		out.flush();
-		out.close();
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
