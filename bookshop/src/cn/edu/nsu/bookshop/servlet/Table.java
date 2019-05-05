442cc83f779146ccce98c8868a501a31d24d1d4f
XuKanglei <xukanglei@126.com>
2019-05-05 09:58:56 +0800

    UPDATE20190505

--- bookshop/src/cn/edu/nsu/bookshop/servlet/Table.java
+++ bookshop/src/cn/edu/nsu/bookshop/servlet/Table.java
@@ -1,6 +1,8 @@
 package cn.edu.nsu.bookshop.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,13 +13,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class table
  */
 //@WebServlet("/table")
-public class table extends HttpServlet {
+public class Table extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public table() {
+    public Table() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,8 +28,34 @@ public class table extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset=\"utf-8\">");
+		out.println("<title>±í¸ñ</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<table border=\"1\" align=\"center\" width=\"90%\">");
+		
+		String nStr = request.getParameter("n");
+		int n = Integer.parseInt(nStr);
+		for(int i = 0; i < n; i++) {
+			out.println("<tr>");
+			for(int j = 0; j < 10; j++) {
+				out.println("<td>");
+				out.println(i * 10 + j + 1);
+				out.println("</td>");
+			}
+			out.println("</tr>");
+		}
+		out.println("</table>");
+		out.println("</body>");
+		out.println("</html>");
+		
+		out.flush();
+		out.close();
 	}
 
 	/**
